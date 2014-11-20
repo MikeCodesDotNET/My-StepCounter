b4b6d05857769867246efc8b6b62c3dae6e7f269
James Montemagno <james.montemagno@gmail.com>
2014-11-19 21:51:05 -0800

    Formatting fixes

--- StepCounter.Android/Helpers/Utils.cs
+++ StepCounter.Android/Helpers/Utils.cs
@@ -41,9 +41,9 @@ namespace StepCounter.Helpers
 		{
 			get
 			{
-                string day = CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(DateTime.Now.DayOfWeek);
-				string month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Now.Month);
-				int dayNum = DateTime.Now.Day;
+        var day = CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(DateTime.Now.DayOfWeek);
+				var month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Now.Month);
+				var dayNum = DateTime.Now.Day;
 				if(Helpers.Settings.UseKilometeres)
 					return day + " " + dayNum + " " + month;
 
